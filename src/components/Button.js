6f5cfffdba30e809957542a7caa9974b41682589
valerie <valerie.andriani@gmail.com>
2020-07-08 10:03:48 +0200

    Adding react-toastify for notifications

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { useHistory } from "react-router-dom";
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
+toast.configure()
 export default ({ task, completeTask, deleteTask }) => {
     const handleCompleteClick = () => { completeTask(task) }
     const handleDeleteClick = () => { deleteTask(task) }
+    const { push: navigate } = useHistory();
+    const notifyComplete = () =>
+        toast.info("Task completed !", {
+            position: toast.POSITION.TOP_RIGHT,
+            autoClose: false
+        });
 
-return <>
-    <button onClick={handleCompleteClick}>Complete</button>
-    <button onClick={handleDeleteClick}>Delete</button>
-</>
+    const notifyDelete = () =>
+        toast.warn("Task deleted !", {
+            position: toast.POSITION.TOP_RIGHT,
+            autoClose: false
+        });
+    return <>
+        <button onClick={() => {
+            handleCompleteClick();
+            navigate("/");
+            notifyComplete();
+        }}
+        > Complete</button>
+        <button onClick={() => {
+            handleDeleteClick();
+            notifyDelete();
+        }}>Delete</button>
+
+    </>
 }
